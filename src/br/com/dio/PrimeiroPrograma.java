beff4298eb3e193fa8aec0234234af96aaafa612
jorgemendees <jorge93_mendes@hotmail.com>
2021-08-10 15:04:14 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*int a = 3;
+		int b = 2;
+		System.out.println("Hello World! " + (a+b));*/
+	}
+}
+
+class Livros{
+	private String nome;
+	private String npag;
+}
